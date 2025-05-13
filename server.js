38f7eac159b3d7dbf57f166866d0ea7d3841de20
lewis-developer-33 <shadowvictor33@gmail.com>
2025-05-13 12:46:19 +0300

    goo

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const port = 3000
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/',(req,res) => {
+    console.log("hi");
+    res.json({
+        status:"success"
+    })
+})
+
+
+app.post('/ussd', (req, res) => {
+    try {
+        console.log(req.body);
+        
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+
+app.listen(port,() => {
+    console.log(`USSD running on port ${port}`);
+})
